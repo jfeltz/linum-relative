e657a0fee7095356daa70de3cb5c015aebc3d0e4
coldnew <coldnew.tw@gmail.com>
2013-12-28 14:25:30 +0800

    tmp add for test

--- linum-relative.el
+++ linum-relative.el
@@ -34,11 +34,11 @@
 
 ;; If you have `melpa` and `emacs24` installed, simply type:
 ;;
-;; 	M-x package-install linum-relative
+;;      M-x package-install linum-relative
 ;;
 ;; In your .emacs
 ;;
-;; 	(require 'linum-relative)
+;;      (require 'linum-relative)
 
 ;;; Code:
 
@@ -56,6 +56,16 @@
   :group 'linum-relative)
 
 ;;;; Customize Variables
+
+(defcustom linum-relative-layout 'left
+  "Layout to use for linum-relative. Currently we has following layout:
+
+left   - Show linum-relative on left, this option will overrite linum-mode. (default)
+right  - Show linum-relative on right. NOTE: This option currently work the same as left-right.
+left-right - Show linum-relative on both left and show linum on right fringe.
+"
+  :type 'symbol
+  :group 'linum-relative)
 
 (defcustom linum-relative-current-symbol "0"
   "The symbol you want to show on the current line, by default it is 0.
@@ -81,23 +91,65 @@ linum-releative will show the real line number at current line."
 
 ;;;; Advices
 (defadvice linum-update (before relative-linum-update activate)
-  "This advice get the last position of linum."
-  (setq linum-relative-last-pos (line-number-at-pos)))
+  "Update last position of linum-relative. If use `right'
+or `left-right' layout, create right fringe."
+  (let* ((window (get-buffer-window))
+         (width (car (window-margins window))))
+    ;; If user use linum-relative right or left-right layout,
+    ;; set the margin on right fringe and make it has same width as left
+    ;; margin.
+    (if (or (eq 'right linum-relative-layout)
+            (eq 'left-right linum-relative-layout))
+        (progn
+          (linum-relative-update-right-fringe (line-number-at-pos))
+          (set-window-margins window width width))
+      (set-window-margins window 0 0))
+    ;; Update last position of linum-relative
+    (setq linum-relative-last-pos (line-number-at-pos))))
+
+(defadvice linum-delete-overlays (after linum-relative-right-delete activate)
+  "Remove right fringe when deactive linum-mode."
+  (when (or (eq 'right linum-relative-layout)
+            (eq 'left-right linum-relative-layout))
+    (set-window-margins (get-buffer-window) 0 0)))
 
 ;;;; Functions
 (defun linum-relative (line-number)
+  "Show relative line-number on left fringe."
   (let* ((diff1 (abs (- line-number linum-relative-last-pos)))
-	 (diff (if (minusp diff1)
-		   diff1
-		 (+ diff1 linum-relative-plusp-offset)))
-	 (current-p (= diff linum-relative-plusp-offset))
-	 (current-symbol (if (and linum-relative-current-symbol current-p)
-			     (if (string= "" linum-relative-current-symbol)
-				 (number-to-string line-number)
-			       linum-relative-current-symbol)
-			   (number-to-string diff)))
-	 (face (if current-p 'linum-relative-current-face 'linum)))
+         (diff (if (minusp diff1)
+                   diff1
+                 (+ diff1 linum-relative-plusp-offset)))
+         (current-p (= diff linum-relative-plusp-offset))
+         (current-symbol (if (and linum-relative-current-symbol current-p)
+                             (if (string= "" linum-relative-current-symbol)
+                                 (number-to-string line-number)
+                               linum-relative-current-symbol)
+                           (number-to-string diff)))
+         (face (if current-p 'linum-relative-current-face 'linum)))
     (propertize (format linum-relative-format current-symbol) 'face face)))
+
+(defun linum-relative-update-right-fringe (line-number)
+  "Update relative numbers on the right margin."
+  (dolist (ov (overlays-in (window-start) (window-end)))
+    (let* ((show-relative-p (if (eq linum-format 'relative) nil t))
+	   (str (overlay-get ov 'linum-str))
+	   (face 'linum-relative-current-face)
+	   (nstr (if str
+		     (number-to-string
+		      (abs (- (string-to-number str) line-number)))))
+	   (nstr2 (if nstr
+		      (if (eq 'linum-relative linum-format)
+			  (number-to-string
+			   (+ line-number (string-to-number nstr)))
+			nstr))))
+      (when nstr2
+	;; copy string properties
+	(set-text-properties 0 (length nstr2) (text-properties-at 0 str) nstr2)
+	(overlay-put ov 'after-string
+		     (propertize " " 'display `((margin right-margin) ,nstr2)))
+;;	(overlay-put ov 'face face)
+	))))
 
 (defun linum-relative-toggle ()
   "Toggle between linum-relative and linum."
@@ -110,3 +162,6 @@ linum-releative will show the real line number at current line."
 
 (provide 'linum-relative)
 ;;; linum-relative.el ends here.
+
+;;(setq linum-relative-layout 'left)
+(setq linum-relative-layout 'right)
